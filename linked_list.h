af61dd0edf9bbe4305a0caf0d358c864c4ae8c58
Tammy Chen <tchen00@stuy.edu>
2019-10-29 10:53:30 -0400

    begin to change song_node

--- linked_list.h
+++ linked_list.h
@@ -1,9 +1,12 @@
 struct song_node {
+  char name[100];
+  char artist[100];
   int i;
   struct song_node * next;
 };
 
 void print_list(struct song_node * n);
 struct song_node * insert_front(struct song_node * n, int k);
+struct song_node * insert_order(struct song_node * n, int k);
 struct song_node * free_list(struct song_node * n);
 struct song_node * remove_song_node(struct song_node * front, int data);
